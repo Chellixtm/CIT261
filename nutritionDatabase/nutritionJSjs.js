d7fda5c1cfb24e139a99d6ee59b04ee0295a6272
Chellixtm <mitch.hudson18@gmail.com>
2019-04-08 18:06:42 -0600

    Final Push
    Estimated final push with completed final project.

--- nutritionDatabase/nutritionJSjs.js
+++ nutritionDatabase/nutritionJSjs.js
@@ -13,17 +13,29 @@ const endSearchURL =
 let foodList = [];
 
 //add event listener to search database button
-document
-  .getElementById("searchDatabase")
-  .addEventListener("touchend", function() {
+document.getElementById("searchDatabase").addEventListener("touchend", function() {
     searchAPI();
   });
+
+  //add event listener for back button
+document.getElementById('backToList').addEventListener("click", () => {
+    let refineButtons = document.getElementById("refine");
+    let list = document.getElementById("result");
+    let foodDetails = document.getElementById("foodDetails");
+    let searchBar = document.getElementById("searchBar");
+    let button = document.getElementById('backToList');
+    
+    list.style.transform = "translateX(0vw)";
+    refineButtons.style.transform = "translateX(0vw)";
+    searchBar.style.transform = "translateX(0vw)";
+    foodDetails.style.transform = "translateX(100vw)";
+    button.style.transform = "translateX(100vw)";
+  })
 
 //function to call an api search
 async function searchAPI() {
   const searchObject = document.getElementById("foodSearch");
   let searchURL = baseSearchURL + "&q=" + searchObject.value + endSearchURL;
-  console.log(searchURL);
   const JSONresponse = await fetch(searchURL)
     .then(response => {
       if (response.ok) {
@@ -34,16 +46,11 @@ async function searchAPI() {
     .then(results => {
       return results.json();
     });
-  //.then(async logSearch => {
   foodList = JSONresponse.list.item;
   foodList = await getMacroDetails(foodList);
-  console.log(foodList);
-  // )
-  // .catch(error => {
-  //   console.log("There was a problem with your fetch:", error.message);
-  // });
 }
 
+//create function to grab food details based on search
 async function getMacroDetails(foodList) {
   let foodListElement = document.getElementById("result");
   foodListElement.innerHTML = "";
@@ -52,11 +59,23 @@ async function getMacroDetails(foodList) {
     let food = foodList[i];
     let foodUrl = baseReportURL + `&ndbno=${food.ndbno}` + endReportURL;
     const getMacros = await grabFoodDetails(foodUrl);
+    console.log(foodUrl);
     const nutrients = getMacros.foods[0].food.nutrients;
     food.calories = findNextMac(nutrients, "Energy");
     food.fat = findNextMac(nutrients, "Total lipid (fat)");
     food.carbs = findNextMac(nutrients, "Carbohydrate, by difference");
     food.protein = findNextMac(nutrients, "Protein");
+    food.fiber = findNextMac(nutrients, "Fiber, total dietary");
+    food.calcium = findNextMac(nutrients, "Calcium, Ca");
+    food.iron = findNextMac(nutrients, "Iron, Fe");
+    food.potassium = findNextMac(nutrients, "Potassium, K");
+    food.sodium = findNextMac(nutrients, "Sodium, Na");
+    food.vitc = findNextMac(nutrients, "Vitamin C, total ascorbic acid");
+    food.saturated = findNextMac(nutrients, "Fatty acids, total saturated");
+    food.mono = findNextMac(nutrients, "Fatty acids, total monosaturated");
+    food.poly = findNextMac(nutrients, "Fatty acids, total polysaturated");
+    food.trans = findNextMac(nutrients, "Fatty acids, total trans");
+    food.chol = findNextMac(nutrients, "Cholesterol");
     foodListElement.appendChild(renderFood(food));
     console.log(food);
     newList.push(food);
@@ -88,27 +107,69 @@ function findNextMac(nutrients, query) {
   }
   const match = nutrients.find(macro => macro.name === query);
   if (match) {
-    return parseFloat(match.value);
+    return match.value + " " + match.unit;
   } else {
-    return "";
+    return "0";
   }
 }
 
 //function to create each food element in returned array
 function renderFood(food) {
   const item = document.createElement("div");
-  item.setAttribute("class", "resultStyle");
-  item.setAttribute("id", "result");
+  item.setAttribute("id", "resultRender");
   item.innerHTML = `
         
         <h2>Name: ${food.name}</h2>
-        <p>Calories: ${food.calories} kcal</p>
-        <p>Fat: ${Math.round(food.fat)} g</p>
-        <p>Carbs: ${Math.round(food.carbs)} g</p>
-        <p>Protein: ${Math.round(food.protein)} g</p>
+        <p>
+        <b>Calories:</b> ${food.calories} <br>
+        <b>Fat:</b> ${food.fat} <br>
+        <b>Carbs:</b> ${food.carbs} <br>
+        <b>Protein:</b> ${food.protein}
+        </p>
     `;
+  item.addEventListener("click", () => {
+    let refineButtons = document.getElementById("refine");
+    let list = document.getElementById("result");
+    let foodDetails = document.getElementById("foodDetails");
+    let searchBar = document.getElementById("searchBar");
+    let button = document.getElementById('backToList');
+
+    list.style.transform = "translateX(-100vw)";
+    refineButtons.style.transform = "translateX(-100vw)";
+    searchBar.style.transform = "translateX(-100vw)";
+    foodDetails.style.transform = "translateX(0vw)";
+    button.style.transform = "translateX(0vw)";
+    foodDetails.innerHTML = renderFoodDetails(food);
+
+  });
   return item;
 }
+
+//creat function to render details for food
+function renderFoodDetails(food) {
+  return `
+  <div class="nameNutrient"><h1>${food.name}</h1></div>
+  <div class="labelNutrient">
+    <div class="headNutrient">Nutrition Facts</div>
+    <div class="majorNutrient" style="font-size:2em"><b>Calories:</b> <span class="right">${food.calories} </span><br></div>
+    <div class="majorNutrient"><b>Total Fat:</b> <span class="right">${food.fat} </span><br></div>
+    <div class="minorNutrient">Saturated Fat: <span class="right">${food.saturated} </span><br></div>
+    <div class="minorNutrient">Monosaturated Fat: <span class="right">${food.mono} </span><br></div>
+    <div class="minorNutrient">Polysaturated Fat: <span class="right">${food.poly} </span><br></div>
+    <div class="minorNutrient"><i>Trans</i> Fat: <span class="right">${food.trans} </span><br></div>
+    <div class="majorNutrient"><b>Cholesterol:</b> <span class="right">${food.chol} </span><br></div>
+    <div class="majorNutrient"><b>Sodium:</b> <span class="right">${food.sodium} </span><br></div>
+    <div class="majorNutrient"><b>Total Carbohydrates:</b> <span class="right">${food.carbs} </span><br></div>
+    <div class="minorNutrient">Fiber: <span class="right">${food.fiber} </span><br></div>
+    <div class="majorNutrient"><b>Protein:</b> <span class="right">${food.protein} </span><br></div>
+    <div class="vitaminNutrient">Vitamin C: <span class="right">${food.vitc} </span><br></div>
+    <div class="vitaminNutrient">Calcium: <span class="right">${food.calcium} </span><br></div>
+    <div class="vitaminNutrient">Iron: <span class="right">${food.iron} </span><br></div>
+    <div class="vitaminNutrient">Potassium: <span class="right">${food.potassium} </span><br></div>
+  </div>
+  `;
+}
+
 
 //Create functions to sort food based on macronutrients
 
